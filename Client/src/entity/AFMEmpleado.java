6c36d995aa147f68cc66ec298749be0e09c47c68
Pablo Caballero <deidigitalis@gmail.com>
2017-06-02 21:45:36 +0200

    Clean up in entity ADMEmpleado-

--- Client/src/entity/AFMEmpleado.java
+++ Client/src/entity/AFMEmpleado.java
@@ -4,11 +4,9 @@ import java.io.Serializable;
 
 import java.util.ArrayList;
 import java.util.Date;
-
 import java.util.List;
 
 import javax.persistence.CascadeType;
-import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -23,101 +21,40 @@ import javax.persistence.Transient;
 import javax.persistence.Version;
 
 @Entity
-@NamedQueries({ @NamedQuery(name = "AFMEmpleado.findAll", query = "select o from AFMEmpleado o"), @NamedQuery(name = "AFMEmpleado.findAllOrderBySalary", query = "select o from AFMEmpleado o order by salario desc") })
+@NamedQueries({ @NamedQuery(name = "AFMEmpleado.findAll", query = "select o from AFMEmpleado o"),
+                @NamedQuery(name = "AFMEmpleado.findAllOrderBySalary",
+                            query = "select o from AFMEmpleado o order by salario desc")
+    })
 public class AFMEmpleado implements Serializable {
     private static final long serialVersionUID = 3899950953404950296L;
+
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO) 
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
     @Version
     private Integer version;
-    
-    
+
     private String apellido;
+
     private String nombre;
-    
-    @OneToOne(cascade=CascadeType.ALL)
+
+    @OneToOne(cascade = CascadeType.ALL)
     private AFMSeguroSanitario seguroSanitario;
-
-    public void setSeguroSanitario(AFMSeguroSanitario seguroSanitario) {
-        this.seguroSanitario = seguroSanitario;
-    }
-
-    public AFMSeguroSanitario getSeguroSanitario() {
-        return seguroSanitario;
-    }
 
     @Transient
     private String nombreCompleto;
 
     @OneToMany(cascade = CascadeType.PERSIST, mappedBy = "empleado")
     private List<AFMNumeroTelefono> telefonos;
-    
+
     @ManyToMany
     private List<AFMProyecto> proyectos = new ArrayList<AFMProyecto>();
-
-    public void setProyectos(List<AFMProyecto> proyectos) {
-        this.proyectos = proyectos;
-    }
-
-    public List<AFMProyecto> getProyectos() {
-        return proyectos;
-    }
-
-    public void setTelefonos(List<AFMNumeroTelefono> telefonos) {
-        this.telefonos = telefonos;
-    }
-
-    public List<AFMNumeroTelefono> getTelefonos() {
-        return telefonos;
-    }
-
-    public void setNombreCompleto(String nombreCompleto) {
-        this.nombreCompleto = nombreCompleto;
-    }
-
-    public String getNombreCompleto() {
-        return nombreCompleto;
-    }
 
     @Temporal(javax.persistence.TemporalType.DATE)
     private Date fechaalta;
 
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setFechaalta(Date fechaalta) {
-        this.fechaalta = fechaalta;
-    }
-
-    public Date getFechaalta() {
-        return fechaalta;
-    }
-
-    public void setSalario(Long salario) {
-        this.salario = salario;
-    }
-
-    public Long getSalario() {
-        return salario;
-    }
     private Long salario;
-
-
 
     public AFMEmpleado() {
     }
@@ -137,9 +74,74 @@ public class AFMEmpleado implements Serializable {
     public void setVersion(Integer version) {
         this.version = version;
     }
-    
-    public String toString(){
-        return "Id: "+ id + " Salario: "+ salario + " Nombre: "+nombre + " Apellidos: "+ apellido + " Fecha de alta: "+fechaalta + " Nombre Completo: "+nombreCompleto /*+"\nTelefonos: "+telefonos + 
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public AFMSeguroSanitario getSeguroSanitario() {
+        return seguroSanitario;
+    }
+
+    public void setSeguroSanitario(AFMSeguroSanitario seguroSanitario) {
+        this.seguroSanitario = seguroSanitario;
+    }
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    public List<AFMNumeroTelefono> getTelefonos() {
+        return telefonos;
+    }
+
+    public void setTelefonos(List<AFMNumeroTelefono> telefonos) {
+        this.telefonos = telefonos;
+    }
+
+    public List<AFMProyecto> getProyectos() {
+        return proyectos;
+    }
+
+    public void setProyectos(List<AFMProyecto> proyectos) {
+        this.proyectos = proyectos;
+    }
+
+    public Date getFechaalta() {
+        return fechaalta;
+    }
+
+    public void setFechaalta(Date fechaalta) {
+        this.fechaalta = fechaalta;
+    }
+
+    public Long getSalario() {
+        return salario;
+    }
+
+    public void setSalario(Long salario) {
+        this.salario = salario;
+    }
+
+    public String toString() {
+        return "Id: " + id + " Salario: " + salario + " Nombre: " + nombre + " Apellidos: " + apellido +
+               " Fecha de alta: " + fechaalta + " Nombre Completo: " + nombreCompleto /*+"\nTelefonos: "+telefonos +
             " SeguroSanitario:" + seguroSanitario*/;
     }
 }
